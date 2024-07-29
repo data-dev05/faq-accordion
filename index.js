f5805549ccc715cc1108fbd5039ee381b345262c
data-dev05 <169947116+data-dev05@users.noreply.github.com>
2024-07-29 14:20:54 -0600

    final version

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const sections = document.getElementsByTagName("section");
+const sections = [...document.getElementsByTagName("section")];
 
-[...sections].forEach((element) => {
+let indexActiveSection = 0;
+
+sections.forEach((element, index) => {
     const question = element.querySelector("h2");
     const answer = element.querySelector("div");
     const icon = element.querySelector("img");
     question.addEventListener("click", () => {
+        if(indexActiveSection !== index){
+            sections[indexActiveSection].querySelector("div").classList.remove("active");
+            sections[indexActiveSection].querySelector("img").src = "./assets/images/icon-plus.svg";
+        }
+        indexActiveSection = index;
         answer.classList.toggle("active");
         answer.classList.contains("active")
         ? icon.src = "./assets/images/icon-minus.svg"
